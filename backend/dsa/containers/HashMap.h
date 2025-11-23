cb68c8f12c25bf84e856ad02ae724e791d93429c
WaasilaAsif <wasif.bscs24seecs@seecs.edu.pk>
2025-11-23 12:24:29 +0500

    Merged wth Anum's and Aman's branches but it has errors so it will need t be resolved first

--- backend/dsa/containers/HashMap.h
+++ backend/dsa/containers/HashMap.h
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-#pragma once
-#include "DynamicArray.h"
-#include "../utils/Pair.h"
-#include <string>
-
-template <typename K, typename V>
-class HashMap {
-private:
-    int numBuckets;
-    DynamicArray<Pair<K,V>>* buckets;  
-    
-    int hash(const K& key) {
-        return hashHelper(key);
-    }
-    
-    // Helper for int keys
-    int hashHelper(const int& key) {
-        return key % numBuckets;
-    }
-    
-    // Helper for string keys
-    int hashHelper(const std::string& key) {
-        int hash = 0;
-        for (char c : key) hash += c;
-        return hash % numBuckets;
-    }
-
-public:
-    HashMap(int bucketsCount = 10) {
-        numBuckets = bucketsCount;
-        buckets = new DynamicArray<Pair<K,V>>[numBuckets];
-    }
-
-    ~HashMap() {
-        delete[] buckets;
-    }
-
-    void put(const K& key, const V& value) {
-        int index = hash(key);
-        DynamicArray<Pair<K,V>>& bucket = buckets[index];
-
-        // update if key exists
-        for (int i=0; i<bucket.size(); i++) {
-            if (bucket.get(i).first == key) {
-                bucket.get(i).second = value;
-                return;
-            }
-        }
-
-        // otherwise insert new
-        bucket.push_back(Pair<K,V>(key,value));
-    }
-
-    bool contains(const K& key) {
-        int index = hash(key);
-        DynamicArray<Pair<K,V>>& bucket = buckets[index];
-
-        for (int i=0; i<bucket.size(); i++)
-            if (bucket.get(i).first == key)
-                return true;
-
-        return false;
-    }
-
-    V get(const K& key) {
-        int index = hash(key);
-        DynamicArray<Pair<K,V>>& bucket = buckets[index];
-
-        for (int i=0; i<bucket.size(); i++)
-            if (bucket.get(i).first == key)
-                return bucket.get(i).second;
-
-        throw std::out_of_range("Key not found");
-    }
-=======
 #pragma once
 #include "DynamicArray.h"
 #include "../utils/Pair.h"
@@ -231,5 +155,4 @@ public:
         }
         entryCount = 0;
     }
->>>>>>> origin/aman-branch
 };
