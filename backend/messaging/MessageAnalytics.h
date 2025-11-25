b6d0f0cdb7d514c52dc47e1c2d2c29e4969be6fe
WaasilaAsif <wasif.bscs24seecs@seecs.edu.pk>
2025-11-25 18:49:18 +0500

    On hold have to resolve main

--- backend/messaging/MessageAnalytics.h
+++ backend/messaging/MessageAnalytics.h
@@ -1,20 +1,30 @@
 #ifndef MESSAGEANALYTICS_H
 #define MESSAGEANALYTICS_H
-
-#include "ConversationGraph.h"
+using namespace std;
+#include "../dsa/messaging_ds/ConversationGraph.h"
 #include "../dsa/containers/DynamicArray.h"
+#include "../dsa/containers/HashMap.h"
+#include "../utils/Pair.h"
 #include <string>
 
 class MessageAnalytics {
 private:
     ConversationGraph graph;
+    HashMap<string, int> userIdMap;  // Map string userIds to integer IDs
+    HashMap<int, string> reverseMap;  // Map integer IDs back to string userIds
+    int nextUserId;
+    HashMap<string, int> userIdMap;  // Map string userIds to integer IDs
+    HashMap<int, string> reverseMap;  // Map integer IDs back to string userIds
+    int nextUserId;
+
+    int getUserId(const string& userId);  // Helper to get or create user ID
 
 public:
     MessageAnalytics();
 
-    void addMessageInteraction(const std::string& senderId, const std::string& receiverId);
-    DynamicArray<std::string> getMostActiveUsers(int k);
-    int getInteractionCount(const std::string& userId1, const std::string& userId2);
+    void addMessageInteraction(const string& senderId, const string& receiverId);
+    DynamicArray<string> getMostActiveUsers(int k);
+    int getInteractionCount(const string& userId1, const string& userId2);
 };
 
 #endif
