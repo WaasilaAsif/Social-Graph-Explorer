283ff631524bcd8a0d226d0a24d7955e3e0c91bf
PeaceTabahi <aajmal.bscs24seecs@seecs.edu.pk>
2025-11-25 19:23:19 +0500

    added some functions my part needed in hashmap

--- backend/dsa/containers/HashMap.h
+++ backend/dsa/containers/HashMap.h
@@ -8,7 +8,8 @@ class HashMap {
 private:
     int numBuckets;
     DynamicArray<Pair<K,V>>* buckets;  
-    
+    int entryCount;
+
     int hash(const K& key) const {
         return hashHelper(key);
     }
@@ -29,6 +30,7 @@ public:
     HashMap(int bucketsCount = 10) {
         numBuckets = bucketsCount;
         buckets = new DynamicArray<Pair<K,V>>[numBuckets];
+        entryCount = 0;
     }
 
     ~HashMap() {
@@ -49,6 +51,7 @@ public:
 
         // otherwise insert new
         bucket.push_back(Pair<K,V>(key,value));
+        entryCount++;
     }
 
     bool contains(const K& key) const {
@@ -103,5 +106,67 @@ public:
                 callback(bucket.get(j).first, bucket.get(j).second);
             }
         }
+    }
+    //aman added
+    V& getRef(const K& key) {
+        int index = hash(key);
+        DynamicArray<Pair<K,V>>& bucket = buckets[index];
+
+        for (int i=0; i<bucket.size(); i++)
+            if (bucket.get(i).first == key)
+                return bucket.get(i).second;
+
+        throw std::out_of_range("Key not found");
+    }
+    void remove(const K& key) {
+        int index = hash(key);
+        DynamicArray<Pair<K,V>>& bucket = buckets[index];
+
+        for (int i = 0; i < bucket.size(); i++) {
+            if (bucket.get(i).first == key) {
+                // shift
+                for (int j = i; j < bucket.size() - 1; j++)
+                    bucket.get(j) = bucket.get(j + 1);
+
+                bucket.pop_back();
+                entryCount--;
+                return;
+            }
+        }
+
+        throw std::out_of_range("Key not found");
+    }
+    int size() const { return entryCount; }
+    bool isEmpty() const { return entryCount == 0; }
+    DynamicArray<K> keys() const {
+        DynamicArray<K> allKeys;
+
+        for (int i = 0; i < numBuckets; i++) {
+            const DynamicArray<Pair<K,V>>& bucket = buckets[i];
+            for (int j = 0; j < bucket.size(); j++)
+                allKeys.push_back(bucket.get(j).first);
+        }
+
+        return allKeys;
+    }
+
+    DynamicArray<V> values() const {
+        DynamicArray<V> allValues;
+
+        for (int i = 0; i < numBuckets; i++) {
+            const DynamicArray<Pair<K,V>>& bucket = buckets[i];
+            for (int j = 0; j < bucket.size(); j++)
+                allValues.push_back(bucket.get(j).second);
+        }
+
+        return allValues;
+    }
+
+    void clear() {
+        for (int i = 0; i < numBuckets; i++) {
+            while (buckets[i].size() > 0)
+                buckets[i].pop_back();
+        }
+        entryCount = 0;
     }
 };
