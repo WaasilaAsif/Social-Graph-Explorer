7979e72855bfe2790666194e019e945e2ada1181
WaasilaAsif <wasif.bscs24seecs@seecs.edu.pk>
2025-11-25 18:50:32 +0500

    Solved unresolved conflicts

--- backend/dsa/user/UserManager.h
+++ backend/dsa/user/UserManager.h
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-#pragma once
-#include <string>
-#include "User.h"
-#include "../containers/DynamicArray.h"
-#include "../containers/Trie.h"
-#include "../containers/HashMap.h"
-
-class UserManager {
-private:
-    DynamicArray<User> users;
-    
-    Trie usernameTrie;            // for searching by name
-    int getUserIndexById(int id);
-
-public:
-    UserManager();
-    // Add/remove users
-    void addUser(const std::string& name, const std::string& password);
-    bool removeUserById(int id);
-    // Get user
-    User* getUserById(int id);
-    // Search users by name prefix
-    DynamicArray<std::string> searchUsersByPrefix(const std::string& prefix);
-    User*login(const std::string& username, const std::string& password);
-    // Connections
-    void makeConnection(int userAId, int userBId);
-    // Graph-based operations
-    DynamicArray<User*> friendSuggestions(int userId);
-    DynamicArray<User*> shortestPath(int userAId, int userBId);
-};
-=======
 // UserManager.h
 #pragma once
 #include <string>
@@ -72,4 +40,3 @@ public:
     void printNetworkStats();
     User* getMostPopularUser();
 };
->>>>>>> origin/aman-branch
